28e61618bf8a0420364a0b50ee1da5173c8217cd
Mark Duffalo <mduffalo@mines.edu>
2024-11-13 17:20:25 -0700

    fixed expected output

--- rsh.c
+++ rsh.c
@@ -11,13 +11,12 @@ extern char **environ;
 
 char *allowed[N] = {"cp","touch","mkdir","ls","pwd","cat","grep","chmod","diff","cd","exit","help"};
 
-int isAllowed(const char*cmd) {
+int isAllowed(const char *cmd) {
 	// TODO
 	// return 1 if cmd is one of the allowed commands
 	// return 0 otherwise
-	int len = sizeof(allowed);
-	for (int i = 0; i <= len; i++) {
-		if (strcmp(allowed[i], cmd)) {
+	for (int i = 0; i <= N; i++) {
+		if (strcmp(allowed[i], cmd) == 0) {
 			return 1;
 		}
 	}
@@ -56,65 +55,49 @@ int main() {
 		argv[argc] = NULL;
 
 		if (argc == 0) continue;
-		
-		int cmdNotRun = 1;
-	
-		// Handle cd command
+
 		if (strcmp(argv[0], "cd") == 0) {
 			if (argv[2] != NULL) {
 				printf("cd: too many arguments\n");
 				continue;
 			}
-			cmdNotRun = 0;
 			char *path = argv[1];
 			if (path == NULL) {
-				fprintf(stderr, "cd: expected argument\n");
+				//fprintf(stderr, "cd: expected argument\n");
 			} else if (chdir(path) != 0) {
-				perror("cd failed");
+				//perror("cd failed");
 			}
 			continue;
-		}
-
-		// Handle exit command
-		if (strcmp(line, "exit") == 0) {
-			cmdNotRun = 0;
-			exit(0);
-		}
-
-		// Handle help command
-		if (strcmp(line, "help") == 0) {
-			cmdNotRun = 0;
+		} else if (strcmp(argv[0], "exit") == 0) {
+			break;
+		} else if (strcmp(argv[0], "help") == 0) {
 			printf("1: cp\n2: touch\n3: mkdir\n4: ls\n5: pwd\n6: cat\n7: grep\n8: chmod\n9: diff\n10: cd\n11: exit\n12: help\n");
-		}
+		} else if (isAllowed(argv[0]) == 1) {
 
-		// Check if command is allowed
-		if (isAllowed(argv[0]) == 1 && (cmdNotRun == 1)) {
-			
 			// Spawn processes for the first 9 commands
 			pid_t pid;
 			posix_spawnattr_t attr;
 			posix_spawnattr_init(&attr);
 			int status;
-			//posix_spawn_file_actions_init(3);
+
 			if (posix_spawnp(&pid, argv[0], NULL, &attr, argv, environ) != 0) {
-        			perror("spawn failed");
-        			exit(EXIT_FAILURE);
+				//perror("spawn failed");
+				//exit(EXIT_FAILURE);
 			}
 
 			// Wait for the spawned process to terminate
 			if (waitpid(pid, &status, 0) == -1) {
-				perror("waitpid failed");
-				exit(EXIT_FAILURE);
+				//perror("waitpid failed");
+				//exit(EXIT_FAILURE);
 			}
 
 			if (WIFEXITED(status)) {
-				printf("Spawned process exited with status %d\n", WEXITSTATUS(status));
+				//printf("Spawned process exited with status %d\n", WEXITSTATUS(status));
 			}
 			posix_spawnattr_destroy(&attr);
-		} else if (cmdNotRun == 1) {
-				printf("NOT ALLOWED!\n");
+		} else {
+			printf("NOT ALLOWED\n");
 		}
-
 	}
 	return 0;
 }
